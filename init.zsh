4064fb4fd9847b6103cf27ab6d6446eaef6639bf
Antonin Deniau <antonin.deniau@nameshield.net>
2019-04-18 10:43:45 +0000

    Add nvim

--- init.zsh
+++ init.zsh
@@ -10,8 +10,19 @@ git clone https://github.com/Antonin-Deniau/cadriciel ~/cadriciel
 # init zplug
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
+# init vimpluged
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 # init config files
 cp ~/cadriciel/conf/mountpoints.conf.sample ~/cadriciel/conf/mountpoints.conf
+
 ln -s ~/cadriciel/conf/editorconfig ~/.editorconfig
 
+mkdir -p ~/.config/nvim/ 
+ln -s ~/cadriciel/conf/nvimrc ~/.config/nvim/init.vim
+
+
+# Source
 echo "source ~/cadriciel/cadriciel.zsh" >> ~/.zshrc
+
